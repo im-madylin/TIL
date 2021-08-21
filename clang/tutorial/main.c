f025234e29d1e7bda5518a46a4ad75a4345cbad6
im-madylin <im.madylin@gmail.com>
2021-08-21 21:18:20 +0900

    윤성우의 열혈 C프로그래밍 406p 도전2번

--- clang/tutorial/main.c
+++ clang/tutorial/main.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 
 int main(void) {
-    int arr[4][4] = {{1,  2,  3,  4},
-                     {5,  6,  7,  8},
-                     {9,  10, 11, 12},
-                     {13, 14, 15, 16}};
-    int temp[4][4];
+    int d=1; //방향전환용 변수
+    int x=0, y=-1; //x:가로, y:세로
+    int max, jmax; //반복문 실행 조건문
+    int cnt=1; //배열에 채워넣을 숫자
 
-    for (int k = 0; k < 4; k++) {
-        for (int i = 0; i < 4; i++) {
-            for (int j=0; j<4; j++){
-                temp[j][3-i] = arr[i][j];
-            }
+    printf("숫자를 입력하시오:");
+    scanf("%d", &max);
+    int arr[max][max];
+    jmax=max;
+
+    while (jmax >= 0) {
+        for (int i = 0; i < jmax; i++) {
+            y=y+d;
+            arr[x][y]=cnt;
+            cnt++;
         }
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++)
-                printf("%d ", arr[i][j]);
-            printf("\n");
+        jmax--;
+        for (int i = 0; i < jmax; i++) {
+            x=x+d;
+            arr[x][y]=cnt;
+            cnt++;
+        }
+        d=d*-1;
+    }
+    for (x=0 ; x < max; x++) {
+        for (y = 0; y < max; y++) {
+            printf("%3d", arr[x][y]);
         }
         printf("\n");
-
-        for (int i = 0; i < 4; i++) {
-            for (int j=0; j<4; j++)
-                arr[i][j] = temp[i][j];
-        }
     }
+    printf("\n");
     return 0;
 }
